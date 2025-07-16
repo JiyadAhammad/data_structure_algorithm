4d3a98e3573c010f86d1b7a6dc9d712568f18b3c
JiyadAhammad <jiyadahammad99@gmail.com>
2025-07-16 21:51:04 +0530

    done more pattern logic

--- bin/data_structure_algorithms.dart
+++ bin/data_structure_algorithms.dart
@@ -3,7 +3,7 @@ import 'imports.dart';
 void main() {
   // Create nodes
 
-  createLinkedListLearn();
+  pascalsTriangle(4);
 }
 
 void printNewMessage() {
--- bin/patterns/patterns.dart
+++ bin/patterns/patterns.dart
@@ -17,4 +17,63 @@ void squarePattern(int n) {
   }
 }
 
+void floydTriangle(int n) {
+  /*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+  */
+  int value = 0;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      ++value;
+      stdout.write('$value ');
+    }
+    print('');
+  }
+}
 
+void numberPyramid(int n) {
+  /*
+    1
+    121
+    12321
+    1234321
+    123454321
+  */
+  int printValue = 1;
+  int rowCount = 1;
+  for (int i = 1; i <= n; i++) {
+    printValue = 1;
+    for (int j = 1; j <= rowCount; j++) {
+      stdout.write(printValue);
+
+      if (j >= i) {
+        --printValue;
+      } else {
+        ++printValue;
+      }
+    }
+    rowCount += 2;
+    print('');
+  }
+}
+
+void pascalsTriangle(int n) {
+  /*
+    1
+    1 1
+    1 2 1
+    1 3 3 1
+    1 4 6 4 1
+  */
+
+  List<List<int>> triangle = [];
+
+  for (var i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (j == 0) {}
+    }
+  }
+}
