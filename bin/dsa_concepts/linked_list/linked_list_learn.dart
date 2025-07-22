93f053a9363c02c8d2a0cc4d13b81a745163af26
JiyadAhammad <jiyadahammad99@gmail.com>
2025-07-22 16:43:57 +0530

    done probelms like add , insert , delete and search operation in linked list

--- bin/dsa_concepts/linked_list/linked_list_learn.dart
+++ bin/dsa_concepts/linked_list/linked_list_learn.dart
@@ -15,10 +15,20 @@ void createLinkedListLearn() {
   learn.addDataToLinekedList(30);
   learn.pushFront(5);
   learn.pushFront(1);
+  learn.pushBack(35);
+  learn.pushBack(40);
+
   // learn.lowestValue();
   // learn.largest();
 
   // learn.deleteNode(LearnNode(data: 30));
+  learn.printLinkedList();
+  learn.popFront();
+  learn.popBack();
+  learn.popBackNoTail();
+  learn.insertMiddle(25, 3);
+  learn.searchElement(235);
+  learn.insertMiddle(2, 0);
   learn.printLinkedList();
 }
 
@@ -35,9 +45,106 @@ class LinkedListLearn {
       head = tail = newNode;
       return;
     } else {
+      /// case 2 : Non-empty linked list
       newNode.next = head;
       head = newNode;
     }
+  }
+
+  void pushBack(int data) {
+    LearnNode newNode = LearnNode(data: data);
+
+    /// Case :1 if linked list is empty
+    if (head == null) {
+      head = tail = newNode;
+    } else {
+      /// case 2 : Non-empty linked list
+      tail!.next = newNode;
+      tail = newNode;
+    }
+  }
+
+  void popFront() {
+    if (head == null) {
+      return;
+    } else {
+      LearnNode tempNode = head!;
+      head = head!.next;
+      tempNode.next = null;
+    }
+
+    /// temp will automatically remove by garbage collector
+  }
+
+  /// if track tail
+  void popBack() {
+    if (head == null) {
+      return;
+    }
+
+    LearnNode? current = head;
+    while (current != null) {
+      if (current.next == tail) {
+        current.next = null;
+        tail = current;
+      }
+      current = current.next;
+    }
+  }
+
+  void popBackNoTail() {
+    if (head == null) {
+      return;
+    }
+
+    LearnNode? current = head;
+    while (current != null) {
+      if (current.next?.next == null) {
+        current.next = null;
+        // tail = current;
+      }
+      current = current.next;
+    }
+  }
+
+  void insertMiddle(int data, int pos) {
+    if (pos < 0) {
+      return;
+    }
+    if (pos == 0) {
+      pushFront(data);
+      return;
+    }
+
+    LearnNode? current = head;
+
+    for (var i = 0; i < pos - 1; i++) {
+      current = current!.next;
+    }
+
+    LearnNode newNode = LearnNode(data: data);
+
+    newNode.next = current!.next;
+    current.next = newNode;
+  }
+
+  void searchElement(int val) {
+    if (head == null) {
+      return;
+    }
+
+    LearnNode? current = head;
+    int pos = 0;
+    while (current != null) {
+      if (current.data == val) {
+        print('$val exist $pos');
+        return;
+      }
+      pos++;
+      current = current.next;
+    }
+
+    print('element not found');
   }
 
   void addDataToLinekedList(int data) {
