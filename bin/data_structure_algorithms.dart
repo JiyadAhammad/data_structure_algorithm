302b9345b52faf2572292246a75825bd34572b39
JiyadAhammad <jiyadahammad99@gmail.com>
2025-09-24 14:04:00 +0530

    Tested Dart Object comaprison using const and final

--- bin/data_structure_algorithms.dart
+++ bin/data_structure_algorithms.dart
@@ -7,3 +7,21 @@ void main() {
 void printNewMessage() {
   print('Hello world to dart programming');
 }
+
+void testAnimalObject() {
+  const a = Animal('dog');
+  const b = Animal('dog');
+
+  final c = Animal('dog');
+  final d = Animal('dog');
+
+  print('${a == b}. const Constructor');
+  print('${c == d}. final Constructor');
+  print('${b == d}. const vs final Constructor');
+}
+
+class Animal {
+  final String name;
+
+  const Animal(this.name);
+}
